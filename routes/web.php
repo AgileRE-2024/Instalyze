194c94398c9042e1955962bd96053cc7b3b86e7b
Memoreza Sabana <memoreza.sabana-2022@fst.unair.ac.id>
2024-10-22 14:20:00 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Http\Controllers\HashtagController;
-use App\Http\Controllers\HeadlineController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InstagramController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/analyze-profile', [ProfileController::class, 'showAnalyzePage'])->name('analyze.profile');
-Route::get('/analyze-hastag', [HashtagController::class, 'showAnalyzePage'])->name('analyze.hastag');
-Route::get('/analyze-headline', [HeadlineController::class, 'showAnalyzePage'])->name('analyze.headline');
-
+Route::post('/analyze', [InstagramController::class, 'analyze'])->name('analyze');
